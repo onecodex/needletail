28cdf2903eb728c65fe7a6cb8ab6396f6aad0022
Roderick Bovee <roderick@onecodex.com>
2017-02-23 11:14:11 -0800

    More benchmarks and track benchmark times on Circle

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -2,10 +2,15 @@
 extern crate bencher;
 extern crate needletail;
 
+use std::fs::File;
+use std::io::Read;
 use needletail::{fastx, kmer, bitkmer};
 use bencher::Bencher;
 
-fn check_kmer_speed(bench: &mut Bencher) {
+// from Bio.SeqIO import parse
+// n_total = sum([len([k for k in slid_win(i.seq, 31) if set(k).issubset({'A', 'C', 'G', 'T'})]) for i in SeqIO.parse('./tests/data/28S.fasta', 'fasta')])
+
+fn bench_kmer_speed(bench: &mut Bencher) {
     let filename = String::from("tests/data/28S.fasta");
     let ksize = 31;
 
@@ -13,21 +18,24 @@ fn check_kmer_speed(bench: &mut Bencher) {
         let mut n_total = 0;
         let mut n_canonical = 0;
         fastx::fastx_file(&filename[..], |seq| {
-            // let normalized_seq = kmer::normalize(Cow::Borrowed(&seq.1), true);
-            for k in seq.1.windows(ksize) {
+            let normalized_seq = kmer::normalize(&seq.1, true);
+            for k in normalized_seq.windows(ksize) {
                 let l = kmer::canonical(k).into_owned();
+                if !kmer::has_no_n(&l) {
+                    continue;
+                }
                 if l == k {
                     n_canonical += 1;
                 }
                 n_total += 1;
             }
-        });
-        assert_eq!(213703, n_total);
-        assert_eq!(108521, n_canonical);
+        }).unwrap();
+        assert_eq!(718007, n_total);
+        assert_eq!(350983, n_canonical);
     })
 }
 
-fn check_bitkmer_speed(bench: &mut Bencher) {
+fn bench_bitkmer_speed(bench: &mut Bencher) {
     let filename = String::from("tests/data/28S.fasta");
     let ksize = 31;
 
@@ -35,19 +43,85 @@ fn check_bitkmer_speed(bench: &mut Bencher) {
         let mut n_total = 0;
         let mut n_canonical = 0;
         fastx::fastx_file(&filename[..], |seq| {
-            for k in bitkmer::BitKmerIter::new(&seq.1, ksize) {
+            for k in bitkmer::BitNuclKmer::new(&seq.1, ksize, false) {
                 let l = bitkmer::canonical(k);
                 if l == k {
                     n_canonical += 1;
                 }
                 n_total += 1;
             }
-        });
-        assert_eq!(213703, n_total);
-        assert_eq!(108521, n_canonical);
+        }).unwrap();
+        assert_eq!(718007, n_total);
+        assert_eq!(350983, n_canonical);
     })
 }
 
+benchmark_group!(kmers, bench_kmer_speed, bench_bitkmer_speed);
 
-benchmark_group!(benches, check_kmer_speed, check_bitkmer_speed);
-benchmark_main!(benches);
+
+fn bench_fastq_bytes(bench: &mut Bencher) {
+    let filename = String::from("tests/data/PRJNA271013_head.fq");
+    let ksize = 31;
+
+    let mut data: Vec<u8> = vec![];
+    let mut f = File::open(filename).unwrap();
+    f.read_to_end(&mut data);
+
+    bench.iter(|| {
+        let mut n_bases = 0;
+        fastx::fastx_bytes(&data, |seq| {
+            n_bases += seq.1.len();
+        }).unwrap();
+        assert_eq!(250000, n_bases);
+    })
+}
+
+fn bench_fastq_file(bench: &mut Bencher) {
+    let filename = String::from("tests/data/PRJNA271013_head.fq");
+    let ksize = 31;
+
+    // warming up the cache doesn't seem to make the timings more repeatable?
+    // fastx::fastx_file(&filename[..], |seq| { assert!(seq.1.len() > 0) }).unwrap();
+    bench.iter(|| {
+        let mut n_bases = 0;
+        fastx::fastx_file(&filename[..], |seq| {
+            n_bases += seq.1.len();
+        }).unwrap();
+        assert_eq!(250000, n_bases);
+    })
+}
+
+fn bench_fasta_bytes(bench: &mut Bencher) {
+    let filename = String::from("tests/data/28S.fasta");
+    let ksize = 31;
+
+    let mut data: Vec<u8> = vec![];
+    let mut f = File::open(filename).unwrap();
+    f.read_to_end(&mut data);
+
+    bench.iter(|| {
+        let mut n_bases = 0;
+        fastx::fastx_bytes(&data, |seq| {
+            n_bases += seq.1.len();
+        }).unwrap();
+        assert_eq!(738580, n_bases);
+    })
+}
+
+fn bench_fasta_file(bench: &mut Bencher) {
+    let filename = String::from("tests/data/28S.fasta");
+    let ksize = 31;
+
+    // warming up the cache doesn't seem to make the timings more repeatable?
+    // fastx::fastx_file(&filename[..], |seq| { assert!(seq.1.len() > 0) }).unwrap();
+    bench.iter(|| {
+        let mut n_bases = 0;
+        fastx::fastx_file(&filename[..], |seq| {
+            n_bases += seq.1.len();
+        }).unwrap();
+        assert_eq!(738580, n_bases);
+    })
+}
+
+benchmark_group!(fastx, bench_fastq_bytes, bench_fastq_file, bench_fasta_bytes, bench_fasta_file);
+benchmark_main!(kmers, fastx);
