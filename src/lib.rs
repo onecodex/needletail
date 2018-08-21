2366f2720c79e8084d297e8fba432f689a68adb1
Roderick Bovee <roderick@onecodex.com>
2018-08-21 13:59:04 -0700

    Add bz, xz, and zip compression support. Closes #11

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,14 @@
 #![crate_name = "needletail"]
 extern crate memchr;
 
-#[cfg(feature = "gz")]
+#[cfg(feature = "compression")]
 extern crate flate2;
+#[cfg(feature = "compression")]
+extern crate bzip2;
+#[cfg(feature = "compression")]
+extern crate xz2;
+#[cfg(feature = "compression")]
+extern crate zip;
 
 pub mod fastx;
 pub mod kmer;
