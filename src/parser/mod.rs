5ceae983dbf4b6fd3b097d74795ddbf86631085f
Luiz Irber <luizirber@users.noreply.github.com>
2020-07-02 08:51:50 +0000

    Drop Seek bound, and add back stdin support and parsing from a Reader (#46)
    * Use cursor and Box to avoid Seek bound
    
    * use niffler
    
    * fix feature, bring back stdin support, remove unwrap
    
    * use niffler 2.1.1, with MultiGzDecoder as default
    
    * use niffler 2.2.0, and add stdin tests

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,14 +1,7 @@
 //! Handles all the FASTA/FASTQ parsing
 use std::fs::File;
-use std::io::{Read, Seek, SeekFrom};
+use std::io::{stdin, Cursor, Read};
 use std::path::Path;
-
-#[cfg(feature = "compression")]
-use bzip2::read::BzDecoder;
-#[cfg(feature = "compression")]
-use flate2::read::MultiGzDecoder;
-#[cfg(feature = "compression")]
-use xz2::read::XzDecoder;
 
 use crate::errors::ParseError;
 pub use crate::parser::fasta::Reader as FastaReader;
@@ -25,103 +18,42 @@ pub use crate::parser::utils::FastxReader;
 // TODO: for now this drops support for stdin parsing. It could be added back if needed though
 // by adding a method to the readers to set some initial buffer.
 
-fn read_file(mut f: File) -> Result<Box<dyn FastxReader>, ParseError> {
-    let mut first = [0; 1];
-    f.read_exact(&mut first)?;
-    // Back to the beginning of the file
-    f.seek(SeekFrom::Start(0))?;
+/// The main entry point of needletail.
+/// Parses the file given a path and return an iterator-like reader struct.
+/// This automatically detects whether the file is:
+/// 1. compressed: gzip, bz and xz are supported and will use the appropriate decoder
+/// 2. FASTA or FASTQ: the right parser will be automatically instantiated
+/// 1 is only available if the `compression` feature is enabled.
+pub fn parse_fastx_file<P: AsRef<Path>>(path: P) -> Result<Box<dyn FastxReader>, ParseError> {
+    let filename = path.as_ref();
+    let f: Box<dyn Read> = if filename == Path::new("-") {
+        Box::new(stdin())
+    } else {
+        Box::new(File::open(path)?)
+    };
+    parse_fastx_reader(f)
+}
 
+/// Parses any input supporting the Read trait and return an iterator-like reader struct.
+/// This automatically detects whether the input is:
+/// 1. compressed: gzip, bz and xz are supported and will use the appropriate decoder
+/// 2. FASTA or FASTQ: the right parser will be automatically instantiated
+/// 1 is only available if the `compression` feature is enabled.
+pub fn parse_fastx_reader<'a, R: Read + 'a>(
+    rdr: R,
+) -> Result<Box<dyn FastxReader + 'a>, ParseError> {
+    let (mut reader, _) = niffler::get_reader(Box::new(rdr))?;
+
+    let mut first = [0; 1];
+    reader.read_exact(&mut first)?;
+
+    let cursor = Cursor::new(first);
     match first[0] {
-        b'>' => Ok(Box::new(FastaReader::new(f))),
-        b'@' => Ok(Box::new(FastqReader::new(f))),
+        b'>' => Ok(Box::new(FastaReader::new(cursor.chain(reader)))),
+        b'@' => Ok(Box::new(FastqReader::new(cursor.chain(reader)))),
         _ => Err(ParseError::new_unknown_format(first[0])),
     }
 }
-
-/// The main entry point of needletail.
-/// Parses the file given a path and return an iterator-like reader struct.
-/// This automatically detects whether the file is:
-/// 1. compressed: gzip, bz and xz are supported and will use the appropriate decoder
-/// 2. FASTA or FASTQ: the right parser will be automatically instantiated
-/// 1 is only available if the `compression` feature is enabled.
-#[cfg(not(feature = "compression"))]
-pub fn parse_fastx_file<P: AsRef<Path>>(path: P) -> Result<Box<dyn FastxReader>, ParseError> {
-    let f = File::open(&path)?;
-    read_file(f)
-}
-
-// Magic bytes for each compression format
-#[cfg(feature = "compression")]
-const GZ_MAGIC: [u8; 2] = [0x1F, 0x8B];
-#[cfg(feature = "compression")]
-const BZ_MAGIC: [u8; 2] = [0x42, 0x5A];
-#[cfg(feature = "compression")]
-const XZ_MAGIC: [u8; 2] = [0xFD, 0x37];
-
-/// The main entry point of needletail.
-/// Parses the file given a path and return an iterator-like reader struct.
-/// This automatically detects whether the file is:
-/// 1. compressed: gzip, bz and xz are supported and will use the appropriate decoder
-/// 2. FASTA or FASTQ: the right parser will be automatically instantiated
-/// 1 is only available if the `compression` feature is enabled.
-#[cfg(feature = "compression")]
-pub fn parse_fastx_file<P: AsRef<Path>>(path: P) -> Result<Box<dyn FastxReader>, ParseError> {
-    let mut f = File::open(&path)?;
-    let mut first = [0; 2];
-    f.read_exact(&mut first)?;
-    // Back to the beginning of the file
-    f.seek(SeekFrom::Start(0))?;
-
-    // Not great to say the least, we load the decoder twice since they don't impl Seek.
-    // That would cause an issue for compressed gzip and is probably not great for performance
-    // Not much compared to decompression overall but still
-    match first {
-        GZ_MAGIC => {
-            let mut gz_reader = MultiGzDecoder::new(f);
-            let mut first = [0; 1];
-            gz_reader.read_exact(&mut first)?;
-            match first[0] {
-                b'>' => Ok(Box::new(FastaReader::new(MultiGzDecoder::new(File::open(
-                    &path,
-                )?)))),
-                b'@' => Ok(Box::new(FastqReader::new(MultiGzDecoder::new(File::open(
-                    &path,
-                )?)))),
-                _ => Err(ParseError::new_unknown_format(first[0])),
-            }
-        }
-        BZ_MAGIC => {
-            let mut bz_reader = BzDecoder::new(f);
-            let mut first = [0; 1];
-            bz_reader.read_exact(&mut first)?;
-            match first[0] {
-                b'>' => Ok(Box::new(FastaReader::new(BzDecoder::new(File::open(
-                    &path,
-                )?)))),
-                b'@' => Ok(Box::new(FastqReader::new(BzDecoder::new(File::open(
-                    &path,
-                )?)))),
-                _ => Err(ParseError::new_unknown_format(first[0])),
-            }
-        }
-        XZ_MAGIC => {
-            let mut xz_reader = XzDecoder::new(f);
-            let mut first = [0; 1];
-            xz_reader.read_exact(&mut first)?;
-            match first[0] {
-                b'>' => Ok(Box::new(FastaReader::new(XzDecoder::new(File::open(
-                    &path,
-                )?)))),
-                b'@' => Ok(Box::new(FastqReader::new(XzDecoder::new(File::open(
-                    &path,
-                )?)))),
-                _ => Err(ParseError::new_unknown_format(first[0])),
-            }
-        }
-        _ => read_file(f),
-    }
-}
-
 // TODO: add a method that parses a string but handles decompression as well?
 
 pub use record::{mask_header_tabs, mask_header_utf8, write_fasta, write_fastq, SequenceRecord};
